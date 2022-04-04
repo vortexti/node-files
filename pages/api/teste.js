44a9a5287cbb80328d43cece38392de58cccabe6
Rui Bernardes Pereira <rui@vortexti.net>
2022-04-04 16:24:33 -0300

    tempo e resposta

--- pages/api/teste.js
+++ pages/api/teste.js
@@ -1,9 +1,12 @@
-function tempo(request, response) {
+async function mikrotik(request, response) {
     const dynamicDate = new Date();
-
+    const mikrotikresponse = await fetch("https://168.228.118.228/rest/interface/ether1");
+    const mikrotikresponseJson = await mikrotikresponse.json();
+    const resposta = mikrotikresponseJson;
     response.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        resposta: resposta
     })
 }
 
-export default tempo;
+export default mikrotik;
