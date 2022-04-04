36190a617bd3e0e6c95b84ca936fbf4dac278597
Rui Bernardes Pereira <rui@vortexti.net>
2022-04-04 16:33:58 -0300

    tempo e resposta2

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -2,14 +2,14 @@ async function mikrotik(request, response) {
   //  const loginmk = process.env.LOGINMIKROTIK;
   //  const senhamk = process.env.SENHAMIKROTIK;
     const dynamicDate = new Date();
- //   const mikrotikresponse = await fetch('https://168.228.118.228/rest/interface/ether1?${loginmk}:${senhamk}');
-  //  const mikrotikresponseJson = await mikrotikresponse.json();
- //   const resposta = mikrotikresponseJson;
+    const mikrotikresponse = await fetch('https://168.228.118.228/rest/interface/ether1');
+    const mikrotikresponseJson = await mikrotikresponse.json();
+    const resposta = mikrotikresponseJson;
     response.json({
         date: dynamicDate.toGMTString(),
-   //     resposta: resposta,
-  //      mikrotikresponse: mikrotikresponse,
-   //     mikrotikresponseJson: mikrotikresponseJson
+        resposta: resposta,
+        mikrotikresponse: mikrotikresponse,
+        mikrotikresponseJson: mikrotikresponseJson
     })
 }
 
